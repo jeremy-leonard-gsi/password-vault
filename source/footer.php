b36e5ee0954f53de3f3f1381cf64e6792a90caad
Jeremy Leonard <jeremyl@gracon.com>
2020-10-26 18:26:46 -0400

    Update footer.php

--- source/footer.php
+++ source/footer.php
@@ -34,7 +34,13 @@
 	</div>
 	<footer id="footer">&copy;Copyright <?=date('Y')?> <?=$site->config->title?></footer>
 </div><!-- Page-->
-	<button class="btn btn-info material-icons-outlined md-inactive" style="right: 0; bottom: 0; position: absolute;" data-toggle="modal" data-target="#debug">bug_report</button>	
+<?php
+    if($site->config->debug){
+        ?>
+    <button class="btn btn-info material-icons-outlined md-inactive" style="right: 0; bottom: 0; position: absolute;" data-toggle="modal" data-target="#debug">bug_report</button>
+    <?php
+    }
+    ?>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 <script src="scripts/pwv.js"></script>
