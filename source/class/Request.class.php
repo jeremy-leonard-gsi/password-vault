d94bee562d856a759249d14a0a8dbb814ea320cb
Jeremy Leonard <jeremyl@gracon.com>
2021-02-22 14:51:08 -0500

    Update Request.class.php

--- source/class/Request.class.php
+++ source/class/Request.class.php
@@ -5,7 +5,12 @@ class Request extends SiteObject{
 	public function __construct() {
 		$this->write(array('get'=>$_GET));
 		$this->write(array('post'=>$_POST));
-		$this->module = trim(parse_url($_SERVER["REQUEST_URI"],PHP_URL_PATH),'/');
+		if(isset($this->config->base)){
+		    error_log($this->config->base);
+    		$this->module = trim(parse_url($_SERVER["REQUEST_URI"],PHP_URL_PATH),'/');
+		}else{
+    		$this->module = trim(parse_url($_SERVER["REQUEST_URI"],PHP_URL_PATH),'/');
+		}
 		$this->action = isset($this->post->action)?$this->post->action:null;
 		$this->method = $_SERVER["REQUEST_METHOD"];							
 	}
