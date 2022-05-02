53130d22b64977fe25710d2c0d5af1e1b35623f4
Jeremy Leonard <jeremyl@gracon.com>
2022-05-02 20:24:59 +0000

    Replace pwvdb.sql

--- source/pwvdb.sql
+++ source/pwvdb.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.5
+-- version 5.0.4deb2
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost
--- Generation Time: Jun 23, 2020 at 07:12 PM
--- Server version: 10.2.31-MariaDB
--- PHP Version: 7.2.5
+-- Host: localhost:3306
+-- Generation Time: May 02, 2022 at 04:22 PM
+-- Server version: 10.5.15-MariaDB-0+deb11u1
+-- PHP Version: 7.4.28
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -21,8 +20,6 @@ SET time_zone = "+00:00";
 --
 -- Database: `pwvdb`
 --
-CREATE DATABASE IF NOT EXISTS `pwvdb` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
-USE `pwvdb`;
 
 -- --------------------------------------------------------
 
@@ -30,10 +27,10 @@ USE `pwvdb`;
 -- Table structure for table `accounts`
 --
 
-DROP TABLE IF EXISTS `accounts`;
 CREATE TABLE `accounts` (
   `accountId` bigint(20) NOT NULL,
   `system` varchar(512) DEFAULT NULL,
+  `url` varchar(2048) DEFAULT NULL,
   `accountName` varchar(254) NOT NULL,
   `accountCreated` timestamp NOT NULL DEFAULT current_timestamp(),
   `accountCreatedBy` varchar(128) NOT NULL,
@@ -43,17 +40,41 @@ CREATE TABLE `accounts` (
   `accountDeleted` tinyint(1) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `accounts`
+--
+
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `config`
 --
 
-DROP TABLE IF EXISTS `config`;
 CREATE TABLE `config` (
   `key` varchar(256) NOT NULL,
   `value` varchar(1024) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `config`
+--
+
+INSERT INTO `config` (`key`, `value`) VALUES
+('apikey', NULL),
+('authLDAPBaseDN', ''),
+('authLDAPBindDN', ''),
+('authLDAPFilter', '(&(objectclass=user)(!(objectclass=computer))(!(UserAccountControl:1.2.840.113556.1.4.803:=2))(|(memberof=CN=Domain Admins,CN=Users,DC=kellogg,DC=kcc)))'),
+('authLDAPFullnameAttribute', 'displayname'),
+('authLDAPSecret', ''),
+('authLDAPURI', ''),
+('authLDAPUserAttribute', 'samaccountname'),
+('authType', 'LDAP'),
+('base', '/password-vault'),
+('enableAPI', '0'),
+('logoURI', ''),
+('pwvPassword', ''),
+('requireSSL', '1'),
+('title', 'Password Vault');
 
 -- --------------------------------------------------------
 
@@ -61,7 +82,6 @@ CREATE TABLE `config` (
 -- Table structure for table `passwords`
 --
 
-DROP TABLE IF EXISTS `passwords`;
 CREATE TABLE `passwords` (
   `passwordId` bigint(20) NOT NULL,
   `accountId` bigint(20) NOT NULL,
@@ -70,6 +90,10 @@ CREATE TABLE `passwords` (
   `passwordCreatedBy` varchar(128) NOT NULL,
   `passwordActive` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `passwords`
+--
 
 --
 -- Indexes for dumped tables
@@ -102,13 +126,13 @@ ALTER TABLE `passwords`
 -- AUTO_INCREMENT for table `accounts`
 --
 ALTER TABLE `accounts`
-  MODIFY `accountId` bigint(20) NOT NULL AUTO_INCREMENT;
+  MODIFY `accountId` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `passwords`
 --
 ALTER TABLE `passwords`
-  MODIFY `passwordId` bigint(20) NOT NULL AUTO_INCREMENT;
+  MODIFY `passwordId` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- Constraints for dumped tables
