4779c8dca5e645c51351036af7c9d958e69a68de
JeremyL <JeremyL@jeremyl-lt-w11.ad.sjredwings.org>
2022-05-10 15:17:05 -0400

    Adding ACL UI

--- source/passwordvault.php
+++ source/passwordvault.php
@@ -307,8 +307,10 @@ include('menu.php');
                             <div class="tab-pane fade" id="editaccesscontrol" role="tabpanel" aria-labeledby="editaccesscontrol-tab">
                                 <?php
                                 foreach(array_intersect(explode(';',$site->config->groupDNs),$_SESSION['groups']) as $group){
+                                    echo '<div class="form-check">';
                                     echo sprintf('<input class="form-check-input" type="checkbox" name="acl[]" value="%s" id="%s">',$group,$group);
                                     echo sprintf('<label class="form-check-label" for="%s">%s</label>',$group,$group);
+                                    echo '</div>';
                                 }
                                 ?>
                             </div>
