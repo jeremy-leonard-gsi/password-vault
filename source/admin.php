1c02ac4539174710c18bda09f9ef66b007a253c8
JeremyL <JeremyL@jeremyl-lt-w11.ad.sjredwings.org>
2022-05-11 10:24:57 -0400

    Started work on the admin module.

--- source/admin.php
+++ source/admin.php
@@ -6,7 +6,7 @@ print_r($site->config);
 
 echo '<table class="table">';
 
-foreach($site->config as $key => $value){
+foreach(sort((array)$site->config) as $key => $value){
     if(!in_array($key, $site->config->hiddenFields)){
        echo sprintf('<tr><th>%s</th><td>%s</td></tr>',$key,$value);
     }
