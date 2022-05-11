8f0f3e3ae0fe5299cad592e97ad62d67ed7ac387
JeremyL <JeremyL@jeremyl-lt-w11.ad.sjredwings.org>
2022-05-11 12:18:56 -0400

    Started work on the admin module.

--- source/menu.php
+++ source/menu.php
@@ -13,6 +13,8 @@
 		<div class="dropdown">
 			<button class="btn pl-2 material-icons-outlined md-36 md-light md-inactive" data-toggle="dropdown" type="button">account_circle</button>
 			<div class="dropdown-menu dropdown-menu-right">
+                                <a class="dropdown-item" href="<?=$site->config->base?>">Password Vault</a> 
+                                <a class="dropdown-item" href="<?=$site->config->base?>/admin">Admin</a> 
 				<form method="post" action="<?=$site->config->base?>/login">
 					<input type="hidden" name="action" value="logout">
 					<button class="dropdown-item" type="submit">Logout</button>
