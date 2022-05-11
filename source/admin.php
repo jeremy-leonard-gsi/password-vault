2a88c44fdf947b6e238cfef12a39f4ee9a9595ce
JeremyL <JeremyL@jeremyl-lt-w11.ad.sjredwings.org>
2022-05-11 10:59:13 -0400

    Started work on the admin module.

--- source/admin.php
+++ source/admin.php
@@ -2,14 +2,19 @@
 include 'header.php';
 include 'menu.php';
 
-print_r($site->config);
+foreach($site->config as $key => $value){
+    if(!is_array($value)){
+        $keys[$key] = $value;
+    }
+}
+foreach($site->config->encodedFields as $key){
+    $keys[$key]=$site->config->$key;
+}
 
 echo '<table class="table">';
 
-foreach($site->config as $key => $value){
-    if(!in_array($key, $site->config->hiddenFields)){
-       echo sprintf('<tr><th>%s</th><td>%s</td></tr>',$key,$value);
-    }
+foreach($keys as $key => $value){
+   echo sprintf('<tr><th>%s</th><td>%s</td></tr>',$key,$value);
 }
 echo '</table>';
 
