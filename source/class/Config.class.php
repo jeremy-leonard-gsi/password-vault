9e53a0361ebdae5b4e0581ce0ad116aabc428187
JeremyL <JeremyL@jeremyl-lt-w11.ad.sjredwings.org>
2022-05-11 10:21:55 -0400

    Started work on the admin module.

--- source/class/Config.class.php
+++ source/class/Config.class.php
@@ -3,9 +3,6 @@
 class Config extends SiteObject{
 
 	private $db;
-        private $pwvPassword;
-        private $pwvSecret;
-        private $authLDAPSecret;
         
         private $hiddenFields;
 
