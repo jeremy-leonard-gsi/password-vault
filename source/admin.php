0dfa0985005e7af22267ccaf9da0889b2e905d15
JeremyL <JeremyL@jeremyl-lt-w11.ad.sjredwings.org>
2022-05-11 09:41:14 -0400

    Started work on the admin module.

--- source/admin.php
+++ source/admin.php
@@ -1,4 +1,11 @@
 <?php
 include 'header.php';
 include 'menu.php';
+
+echo '<table class="table">';
+foreach($site->config as $key => $value){
+       echo sprintf('<tr><th>%s</th><td>%s</td></tr>',$key,$value);
+}
+echo '</table>';
+
 include 'footer.php';
