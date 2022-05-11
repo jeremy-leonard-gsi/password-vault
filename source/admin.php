d113c53d5ab60145ad88d9630b0d56fbb354dd28
JeremyL <JeremyL@jeremyl-lt-w11.ad.sjredwings.org>
2022-05-11 09:47:17 -0400

    Started work on the admin module.

--- source/admin.php
+++ source/admin.php
@@ -2,12 +2,14 @@
 include 'header.php';
 include 'menu.php';
 
-print_r(array_diff((array)$site->config, $_CONFIG));
+//print_r(array_diff((array)$site->config, $_CONFIG));
 
 echo '<table class="table">';
-//foreach(array_diff((array)$site->config, $_CONFIG) as $key => $value){
-//       echo sprintf('<tr><th>%s</th><td>%s</td></tr>',$key,$value);
-//}
+foreach($site->config as $key => $value){
+    if($key != 'db'){
+       echo sprintf('<tr><th>%s</th><td>%s</td></tr>',$key,$value);
+    }
+}
 echo '</table>';
 
 include 'footer.php';
