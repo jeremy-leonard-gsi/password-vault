5d85b562220ec01bf12d5a4e52f4bc24bb615b96
JeremyL <JeremyL@jeremyl-lt-w11.ad.sjredwings.org>
2022-05-11 12:21:38 -0400

    Started work on the admin module.

--- source/menu.php
+++ source/menu.php
@@ -13,12 +13,12 @@
 		<div class="dropdown">
 			<button class="btn pl-2 material-icons-outlined md-36 md-light md-inactive" data-toggle="dropdown" type="button">account_circle</button>
 			<div class="dropdown-menu dropdown-menu-right">
+                               <a class="dropdown-item" href="<?=$site->config->base?>">Password Vault</a>
                                 <?php
                                     if(in_array($site->config->globalAdminGroupDN, $_SESSION['groups'])){
-                                       echo '<a class="dropdown-item" href="<?=$site->config->base?>">Password Vault</a>';
+                                        echo '<a class="dropdown-item" href="<?=$site->config->base?>/admin">Admin</a>';
                                     }
                                     ?>
-                                <a class="dropdown-item" href="<?=$site->config->base?>/admin">Admin</a> 
 				<form method="post" action="<?=$site->config->base?>/login">
 					<input type="hidden" name="action" value="logout">
 					<button class="dropdown-item" type="submit">Logout</button>
